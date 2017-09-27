3527fffc52d179214dc2bd8663f9e7f0a32a1656
jbrooks19 <jbrooks19@wooster.edu>
2017-09-27 11:22:41 +0200

    meaningless

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -24,7 +24,7 @@ public class PauseMenu : MonoBehaviour {
 	}
 
 	void Update(){
-		if (Input.GetKeyDown (KeyCode.Escape)) {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
 			if (GameManager.gameState == GameManager.GameState.Running) {
 				Pause ();
 			}
