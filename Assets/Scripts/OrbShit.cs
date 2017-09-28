31b203951a4ae00ee1d7c9d97888ab6055178aaf
Unknown <marth.hehe@gmail.com>
2017-09-28 12:53:36 +0200

    Started Elemental System

--- Assets/Scripts/OrbShit.cs
+++ Assets/Scripts/OrbShit.cs
@@ -2,22 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbShit : MonoBehaviour {
+public class OrbShit : MonoBehaviour
+{
+	[Header("Sprites")]
+	[SerializeField] Sprite fire;
+	[SerializeField] Sprite ice;
+	[SerializeField] Sprite inert;
 
-	// Use this for initialization
-	void Start () {
+	private string element;
+
+	void Start ()
+	{
 		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
 
-		Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		Vector2 stuff = gameObject.transform.position;
-		Vector2 moreStuff = mouseWorldPos - stuff;
+		Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 localPos = gameObject.transform.position;
+		Vector2 localToMouse = mouseWorldPos - localPos;
 
-		moreStuff.Normalize();
-		rb.AddForce (moreStuff * 500);
+		localToMouse.Normalize();
+		rb.AddForce (localToMouse * 500);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void OnTriggerEnter (Collider other)
+	{
+		/*if (other.gameObject is IElemental)
+		{
+			
+		}*/
+		print("Triggered");
+	}
+
+	public void ChangeElement (string newElement)
+	{
+		element = newElement;
+
+		switch (element)
+		{
+		case ("Fire"):
+			ignite();
+			break;
+		case ("Ice"):
+			freeze();
+			break;
+		default:
+			Debug.LogError("Unexpected element encountered");
+			element = "Inert";
+			revert();
+			break;
+		}
+	}
+
+	private void ignite ()
+	{
 		
+	}
+
+	private void freeze ()
+	{
+		//change material to slide
+	}
+
+	private void revert ()
+	{
+
 	}
 }
--- Assets/Scripts/OrbSpawner.cs
+++ Assets/Scripts/OrbSpawner.cs
@@ -2,21 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbSpawner : MonoBehaviour {
-	public GameObject orb;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class OrbSpawner : MonoBehaviour
+{
+	[SerializeField] GameObject orb;
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
-
-		if (Input.GetMouseButtonDown(0)) {
+	void FixedUpdate()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
 			Instantiate (orb, gameObject.transform);
-
 		}
 	}
 }
