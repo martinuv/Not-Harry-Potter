1da4c3f7ede3ba7222c4fc26e9a9b7c1c4a0725a
jbrooks19 <jbrooks19@wooster.edu>
2017-09-28 19:32:37 +0200

    wizard

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed=10f;
+	bool facingRight = true;
+	Animator anim;
+	private Rigidbody2D rbg;
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+		rbg = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		rbg.velocity = new Vector2 (move * maxSpeed, rbg.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+
+	}
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+
+}
