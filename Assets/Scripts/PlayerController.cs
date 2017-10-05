c66ee05e8c056270ed35da6edbb735e1f524ffcd
jbrooks19 <jbrooks19@wooster.edu>
2017-10-05 10:15:13 +0200

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -62,6 +63,7 @@ public class PlayerController : MonoBehaviour {
 
 	private void Respawn()
 	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		transform.position = curRespawn;
 	}
 
