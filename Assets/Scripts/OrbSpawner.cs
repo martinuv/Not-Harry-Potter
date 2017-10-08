778d0d0e0fd469e1e0d512526043f5434da2dc0d
Unknown <marth.hehe@gmail.com>
2017-10-08 23:38:02 +0200

    Last Commit EVARRRR

--- Assets/Scripts/OrbSpawner.cs
+++ Assets/Scripts/OrbSpawner.cs
@@ -1,30 +1,1 @@
-﻿using System.Collections.Generic;using UnityEngine;public class OrbSpawner : MonoBehaviour{	[SerializeField] Orb orb;	public static List<Orb> orbs = new List<Orb>(6);    private Animator anim;    private bool mAxisInUse = false;    void Awake()    {        anim = GetComponentInParent<Animator>();    }    void Update ()	{
-        if (GameManager.gameState == GameManager.GameState.Running)
-        {
-            if (Input.GetAxisRaw("Fire1") != 0)
-            {
-                if (!mAxisInUse)
-                {
-                    anim.SetTrigger("Cast");
-
-                    Orb newOrb = Instantiate(orb, gameObject.transform);
-                    newOrb.transform.parent = null;
-
-                    orbs.Add(newOrb);
-                    if (orbs.Count > 5)
-                    {
-                        Destroy(orbs[0].gameObject);
-                        orbs.RemoveAt(0);
-                    }
-
-                    mAxisInUse = true;
-                }
-            }
-
-            if (Input.GetAxisRaw("Fire1") == 0)
-            {
-                mAxisInUse = false;
-            }
-        }
-    }
-}
+﻿using System.Collections.Generic;using UnityEngine;public class OrbSpawner : MonoBehaviour{	[SerializeField] Orb orb;	public static List<Orb> orbs = new List<Orb>(6);    private Animator anim;    private bool mAxisInUse = false;    void Awake()    {        anim = GetComponentInParent<Animator>();    }    void Update ()	{        if (GameManager.gameState == GameManager.GameState.Running)        {            if (Input.GetAxisRaw("Fire1") != 0)            {                if (!mAxisInUse)                {                    anim.SetTrigger("Cast");                    Orb newOrb = Instantiate(orb, gameObject.transform);                    newOrb.transform.parent = null;                    orbs.Add(newOrb);                    if (orbs.Count > 5)                    {                        Destroy(orbs[0].gameObject);                        orbs.RemoveAt(0);                    }                    mAxisInUse = true;                }            }            if (Input.GetAxisRaw("Fire1") == 0)            {                mAxisInUse = false;            }        }    }}
